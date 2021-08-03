436564e1ab73d42958c9686fc745822b0be0ce26
Anatoliy_Iliev <vekok12@gmail.com>
2021-08-03 17:48:26 +0300

    day-1

--- src/App.js
+++ src/App.js
@@ -1,5 +1,17 @@
-function App() {
-  return <div></div>;
+import { Component } from "react";
+
+class App extends Component {
+  state = {
+    id: "",
+    webformatURL: "",
+    largeImageURL: "",
+  };
+
+  render() {
+    // const {  } = this.state;
+
+    return <div></div>;
+  }
 }
 
 export default App;
