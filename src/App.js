81f3b6a333d2724a035d5ed7ea9a2d0037556f1f
Anatoliy_Iliev <vekok12@gmail.com>
2021-08-04 17:59:50 +0300

    day-2

--- src/App.js
+++ src/App.js
@@ -1,16 +1,35 @@
 import { Component } from "react";
+import { PixabayAPI } from "./services/PixabayAPI";
 
 class App extends Component {
   state = {
-    id: "",
-    webformatURL: "",
-    largeImageURL: "",
+    searchQuery: "cat",
+    page: 1,
+    error: null,
+    loading: false,
+  };
+
+  loadImage = async (searchQuery, page) => {
+    console.log("loadImage", await PixabayAPI(searchQuery, page));
+    // await PixabayAPI(searchQuery, page)
   };
 
   render() {
-    // const {  } = this.state;
+    const { searchQuery, page } = this.state;
 
-    return <div></div>;
+    return (
+      <div>
+        <h1>How many people</h1>
+
+        {this.loadImage(searchQuery, page)}
+        <ul>
+          <li>
+            {console.log("222", this.loadImage(searchQuery, page))}
+            {/* <img src={this.loadImage.hits[0].webformatURL} alt={this.loadImage.hits[0].id }/> */}
+          </li>
+        </ul>
+      </div>
+    );
   }
 }
 
---
+++ src/services/PixabayAPI.js
@@ -0,0 +1,21 @@
+// const axios = require('axios');
+
+const API_KEY = "21951124-cbc15cb68780ddc893d0875a8";
+const BASE_URL = "https://pixabay.com/api";
+
+// id: "",
+// webformatURL: "",
+// largeImageURL: "",
+
+export const PixabayAPI = async (searchQuery, page) => {
+  console.log("searchQuery", searchQuery);
+  console.log("page", page);
+  const API_URL = `${BASE_URL}/?q=${searchQuery}&page=${page}&key=${API_KEY}&image_type=photo&orientation=horizontal&per_page=12`;
+
+  const response = await fetch(API_URL);
+  if (response.ok) {
+    return response.json();
+  }
+
+  return Promise.reject(new Error(`Поиск ${searchQuery} не дал результата`));
+};
