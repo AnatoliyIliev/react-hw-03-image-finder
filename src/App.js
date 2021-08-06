c7d117bf95e1dc7433e7cedba1f2cf8af2af6dda
Anatoliy_Iliev <vekok12@gmail.com>
2021-08-06 17:52:36 +0300

    day-3

--- src/App.js
+++ src/App.js
@@ -18,10 +18,12 @@ class App extends Component {
   loadImage = async () => {
     const { searchQuery, page } = this.state;
     this.setState({ loading: true });
-    // console.log("loadImage-APP", searchQuery);
+    console.log("loadImage-APP", searchQuery);
     try {
       const PixabayImage = await PixabayAPI(searchQuery, page);
-      this.setState({ PixabayImage: PixabayImage.hits });
+      // this.setState({ PixabayImage: PixabayImage.hits });
+      console.log(PixabayImage.hits);
+      return PixabayImage.hits;
     } catch (error) {
       this.setState({ error: error.message });
     } finally {
@@ -30,25 +32,27 @@ class App extends Component {
   };
 
   submitForm = (searchQuery) => {
-    // console.log("searchQuery-submitForm-App", searchQuery);
     this.setState({ searchQuery });
     // this.loadImage(searchQuery);
-
-    // console.log(this.loadImage().hits)
   };
 
   render() {
     // const { PixabayImage, searchQuery, page } = this.state;
-    // const { PixabayImage, searchQuery } = this.state;
-    const { searchQuery } = this.state;
+    const { PixabayImage, searchQuery } = this.state;
+    // const { searchQuery } = this.state;
     return (
       <div>
-        <Searchbar onSubmit={this.submitForm} />
-        <ImageGallery
-          // loadImage={this.loadImage}
-          PixabayImage={this.loadImage()}
-          searchQuery={searchQuery}
+        <Searchbar
+          onSubmit={this.submitForm}
+          // onClick={this.loadImage}
         />
+        {searchQuery && (
+          <ImageGallery
+            // PixabayImage={this.loadImage}
+            PixabayImage={PixabayImage}
+            searchQuery={searchQuery}
+          />
+        )}
         <ToastContainer autoClose={3000} />
       </div>
     );
--- src/Components/ImageGalleryItem/ImageGalleryItem.js
+++ src/Components/ImageGalleryItem/ImageGalleryItem.js
@@ -1,17 +1,20 @@
-// import styles from './ImageGalleryItem.module.scss';
+import styles from "./ImageGalleryItem.module.scss";
 
 const ImageGalleryItem = (PixabayImage, searchQuery) => {
+  console.log(PixabayImage, searchQuery);
   return (
     PixabayImage.lenght > 0 && (
       <>
-        {
-          console.log(PixabayImage)
-          // PixabayImage.map((hit) => (
-          //   <li className={styles.ImageGalleryItem}>
-          //     <img src={hit.webformatURL} alt={searchQuery} key={ hit.id }className={styles.ImageGalleryItem_image} />
-          //   </li>
-          // ))
-        }
+        {PixabayImage.map((hit) => (
+          <li className={styles.ImageGalleryItem}>
+            <img
+              src={hit.webformatURL}
+              alt={searchQuery}
+              key={hit.id}
+              className={styles.ImageGalleryItem_image}
+            />
+          </li>
+        ))}
       </>
     )
   );
